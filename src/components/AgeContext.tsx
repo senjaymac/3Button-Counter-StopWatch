c6e143c72b542d76c73cab93c30d89c6bc627cb4
Jayson <senjaymac@gmail.com>
2025-10-01 15:30:10 +0800

    Age Counter, Note Pad and Dog Picture App

--- src/components/AgeContext.tsx
+++ src/components/AgeContext.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, useContext, useReducer, ReactNode } from 'react';
+import React, { createContext, useContext, useReducer } from 'react';
+import type { ReactNode } from 'react';
 
 interface AgeState {
   age: number;
